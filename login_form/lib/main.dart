0812df7192d556996ba1288272e9d69632e4b9b7
Dmitry <ldimka@gmail.com>
2021-10-12 22:39:33 +0300

    Update main.dart

--- login_form/lib/main.dart
+++ login_form/lib/main.dart
@@ -6,12 +6,14 @@ void main() {
 }
 
 class LoginApp extends StatelessWidget{
+  //const color_main = const Color(0xff17325c);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Форма вохода',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
       home: LoginPage(title:''),
     );
