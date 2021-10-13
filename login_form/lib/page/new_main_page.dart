b3ca5c192fda063d8a22f4f4e4611c826895afeb
Dmitry <ldimka@gmail.com>
2021-10-13 23:53:53 +0300

    Update new_main_page.dart

--- login_form/lib/page/new_main_page.dart
+++ login_form/lib/page/new_main_page.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:login_form/const/color.dart';
 import 'package:login_form/const/config.dart';
 import 'package:login_form/models/job.dart';
 import 'package:http/http.dart' as http;
+import 'package:login_form/page/main_page.dart';
 
 class NewMainPage extends StatefulWidget {
   static const routeName = "newMainPage";
@@ -24,8 +26,7 @@ class _NewMainPageState extends State<NewMainPage> {
 
     //print(data);
     setState(() {
-      jobList = data['records']
-          .map((data) => Job.fromJson(data)).toList();
+      jobList = data['records'].map((data) => Job.fromJson(data)).toList();
     });
   }
 
@@ -39,40 +40,49 @@ class _NewMainPageState extends State<NewMainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
+        backgroundColor: mainColor,
+        //backgroundColor: Colors.transparent,
         elevation: 0,
         leadingWidth: 20,
+        toolbarHeight: 60,
         leading: IconButton(
-            padding: EdgeInsets.only(left: 20),
-            onPressed: () {},
-            icon: Icon(Icons.arrow_back_ios, color: Colors.grey.shade600,)
-        ),
+            padding: EdgeInsets.all(15),
+            onPressed: ()=>Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(builder: (BuildContext context) => MainPage()),
+                    (Route<dynamic> route) => false),
+            icon: Icon(
+              Icons.arrow_back_ios,
+              color: Colors.white,
+            )),
         actions: [
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(10),
             child: IconButton(
                 iconSize: 30,
                 onPressed: () {},
-                icon: Icon(Icons.notifications_none, color: Colors.grey.shade400,)
-            ),
+                icon: Icon(
+                  Icons.notifications_none,
+                  color: Colors.white,
+                )),
           )
         ],
-        title: Container(
-          height: 45,
-          child: TextField(
-            cursorColor: Colors.grey,
-            decoration: InputDecoration(
-              contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 0),
-              filled: true,
-              fillColor: Colors.grey.shade200,
-              prefixIcon: Icon(Icons.search, color: Colors.grey),
-              border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(50),
-                  borderSide: BorderSide.none
+        title: Center(
+          child: Container(
+            padding: EdgeInsets.only(left: 35),
+            height: 40,
+            child: TextField(
+              cursorColor: Colors.grey,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 0),
+                filled: true,
+                fillColor: Colors.grey.shade200,
+                prefixIcon: Icon(Icons.search, color: Colors.grey),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide.none),
+                hintText: "Поиск заявки",
+                hintStyle: TextStyle(fontSize: 16),
               ),
-              hintText: "Search e.g Software Developer",
-              hintStyle: TextStyle(fontSize: 14),
-
             ),
           ),
         ),
@@ -93,7 +103,7 @@ class _NewMainPageState extends State<NewMainPage> {
       padding: EdgeInsets.all(10),
       margin: EdgeInsets.only(bottom: 15),
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
+          borderRadius: BorderRadius.circular(10),
           color: Colors.white,
           boxShadow: [
             BoxShadow(
@@ -102,37 +112,43 @@ class _NewMainPageState extends State<NewMainPage> {
               blurRadius: 2,
               offset: Offset(0, 1),
             ),
-          ]
-      ),
+          ]),
       child: Column(
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Expanded(
-                child: Row(
-                    children: [
-                      Container(
-                          width: 60,
-                          height: 60,
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(20),
-                            child: Image.asset(job.company),
-                          )
-                      ),
-                      SizedBox(width: 10),
-                      Flexible(
-                        child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(job.name, style: TextStyle(color: Colors.black, fontSize: 15, fontWeight: FontWeight.w500)),
-                              SizedBox(height: 5,),
-                              Text(job.phone, style: TextStyle(color: Colors.grey[500])),
-                            ]
-                        ),
-                      )
-                    ]
-                ),
+                child: Row(children: [
+                  Container(
+                      width: 60,
+                      height: 60,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image.network(
+                            'https://www.behalacollege.in/International_conference/bkend/dist/img/test-img.png'),
+                      )),
+                  SizedBox(width: 10),
+                  Flexible(
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(job.name,
+                              style: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500)),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(job.phone,
+                              style: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  color: Colors.grey[500])),
+                        ]),
+                  )
+                ]),
               ),
               GestureDetector(
                 onTap: () {
@@ -146,42 +162,63 @@ class _NewMainPageState extends State<NewMainPage> {
                     duration: Duration(milliseconds: 300),
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(12),
-                        border: Border.all(color: job.isMyFav ? Colors.red.shade100 : Colors.grey.shade300,)
-                    ),
+                        border: Border.all(
+                          color: job.isMyFav
+                              ? Colors.red.shade100
+                              : Colors.grey.shade300,
+                        )),
                     child: Center(
-                        child: job.isMyFav ? Icon(Icons.favorite, color: Colors.red,) : Icon(Icons.favorite_outline, color: Colors.grey.shade600,)
-                    )
-                ),
+                        child: job.isMyFav
+                            ? Icon(
+                                Icons.add,
+                                color: Colors.red,
+                              )
+                            : Icon(
+                                Icons.add,
+                                color: Colors.grey.shade600,
+                              ))),
               )
             ],
           ),
-          SizedBox(height: 20,),
+          SizedBox(
+            height: 20,
+          ),
           Container(
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Row(
+                Column(
                   children: [
                     Container(
-                      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+                      /*padding:
+                          EdgeInsets.symmetric(vertical: 8, horizontal: 15),
                       decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12),
-                          color: Colors.grey.shade200
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.grey.shade200),*/
+                      child: Text(
+                        job.company,
+                        style: TextStyle(color: Colors.black, fontFamily: 'Montserrat',),
                       ),
-                      child: Text(job.company, style: TextStyle(color: Colors.black),),
                     ),
-                    SizedBox(width: 10,),
+                    //SizedBox(width: 10,),
                     Container(
-                      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+                      margin: EdgeInsets.only(top: 10),
+                      padding:
+                          EdgeInsets.symmetric(vertical: 8, horizontal: 15),
                       decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12),
-                          color: Color(0xffbf2038).withAlpha(20)
+                          borderRadius: BorderRadius.circular(10),
+                          color: Color(0xffbf2038).withAlpha(20)),
+                      child: Text(
+                        job.email,
+                        style: TextStyle(color: Color(0xffbf2038), fontFamily: 'Montserrat',),
                       ),
-                      child: Text(job.email, style: TextStyle(color: Color(0xffbf2038)),),
                     )
                   ],
                 ),
-                Text(job.name, style: TextStyle(color: Colors.grey.shade800, fontSize: 12),)
+                Text(
+                  job.name,
+                  style: TextStyle(color: Colors.grey.shade800, fontSize: 12),
+                )
               ],
             ),
           )
