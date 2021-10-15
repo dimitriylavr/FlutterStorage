d86586708a16e2b7df40dd067f59131be17d45b6
Dmitry <ldimka@gmail.com>
2021-10-15 17:55:51 +0300

    Update main.dart

--- calculator/lib/page/main.dart
+++ calculator/lib/page/main.dart
@@ -7,8 +7,8 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  TextEditingController _heightController = TextEditingController();
-  TextEditingController _weightController = TextEditingController();
+  final TextEditingController _heightController = TextEditingController();
+  final TextEditingController _weightController = TextEditingController();
   double _bmiResult = 0;
   String _textResult = "";
 
@@ -16,10 +16,12 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text(
+          title: const Text(
             "Калькулятор ИМТ",
-            style:
-                TextStyle(color: containerColor, fontWeight: FontWeight.w300),
+            style: TextStyle(
+                fontFamily: 'Montserrat',
+                color: containerColor,
+                fontWeight: FontWeight.w300),
           ),
           backgroundColor: Colors.transparent,
           elevation: 0,
@@ -29,7 +31,7 @@ class _MainPageState extends State<MainPage> {
         body: SingleChildScrollView(
           child: Column(
             children: [
-              SizedBox(
+              const SizedBox(
                 height: 20,
               ),
               Row(
@@ -37,73 +39,108 @@ class _MainPageState extends State<MainPage> {
                 children: [
                   Container(
                     width: 130,
-                    child: TextField(
-                      controller: _heightController,
-                      style: TextStyle(
-                          fontSize: 42,
-                          fontWeight: FontWeight.w300,
-                          color: containerColor),
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Рост",
-                        hintStyle: TextStyle(
-                            fontSize: 42,
-                            fontWeight: FontWeight.w300,
-                            color: Colors.white.withOpacity(.8)),
-                      ),
+                    child: Column(
+                      children: [
+                        TextField(
+                          controller: _heightController,
+                          style: const TextStyle(
+                              fontSize: 42,
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.w300,
+                              color: containerColor),
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Рост",
+                            hintStyle: TextStyle(
+                                fontSize: 42,
+                                fontWeight: FontWeight.w300,
+                                color: Colors.white.withOpacity(.8)),
+                          ),
+                        ),
+                        Text(
+                          "сантиметры",
+                          textAlign: TextAlign.right,
+                          style: TextStyle(
+                              fontSize: 10,
+                              fontFamily: 'Montserrat',
+                              color: Colors.white.withOpacity(.8)),
+                        )
+                      ],
                     ),
                   ),
                   Container(
                     width: 130,
-                    child: TextField(
-                      controller: _weightController,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontSize: 42,
-                          fontWeight: FontWeight.w300,
-                          color: containerColor),
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Вес",
-                        hintStyle: TextStyle(
-                            fontSize: 42,
-                            fontWeight: FontWeight.w300,
-                            color: Colors.white.withOpacity(.8)),
-                      ),
+                    child: Column(
+                      children: [
+                        TextField(
+                          controller: _weightController,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                              fontSize: 42,
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.w300,
+                              color: containerColor),
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Вес",
+                            hintStyle: TextStyle(
+                                fontSize: 42,
+                                fontWeight: FontWeight.w300,
+                                color: Colors.white.withOpacity(.8)),
+                          ),
+                        ),
+                        Text(
+                          "килограммы",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              fontSize: 10,
+                              fontFamily: 'Montserrat',
+                              color: Colors.white.withOpacity(.8)),
+                        )
+                      ],
                     ),
                   ),
                 ],
               ),
               const SizedBox(
-                height: 30,
+                height: 40,
               ),
-              GestureDetector(
-                onTap: () {
+              MaterialButton(
+                onPressed: () {
                   double _h = double.parse(_heightController.text);
                   double _w = double.parse(_weightController.text);
-                  _h = _h / 100;
-                  _h *= _h;
-                  setState(() {
-                    print(_h);
-                    print(_w);
-                    _bmiResult = _w / _h;
-                    if (_bmiResult > 25) {
-                      _textResult = "У тебя лишний вес!";
-                    } else if (_bmiResult >= 18.5 && _bmiResult <= 25) {
-                      _textResult = "Твой вес нормальный!";
-                    } else {
-                      _textResult = "Недостаточный вес!";
-                    }
-                  });
+                    _h = _h / 100;
+                    _h *= _h;
+                    setState(() {
+                      _bmiResult = _w / _h;
+                      if (_bmiResult > 25) {
+                        _textResult = "У тебя лишний вес!";
+                      } else if (_bmiResult >= 18.5 && _bmiResult <= 25) {
+                        _textResult = "Твой вес нормальный!";
+                      } else {
+                        _textResult = "Недостаточный вес!";
+                      }
+                    });
                 },
-                child: const Text(
-                  "Расчитать",
-                  style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
-                      color: containerColor
+                textColor: Colors.white,
+                padding: EdgeInsets.all(0.0),
+                child: Container(
+                  width: 160,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      color: containerColor,
+                      borderRadius: BorderRadius.circular(10),),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Расчитать",
+                        style: TextStyle(
+                            fontSize: 28, color: mainColor, letterSpacing: 1),
+                      ),
+                    ],
                   ),
                 ),
               ),
@@ -112,7 +149,10 @@ class _MainPageState extends State<MainPage> {
               ),
               Text(
                 _bmiResult.toStringAsFixed(2),
-                style: TextStyle(fontSize: 90, color: containerColor),
+                style: TextStyle(
+                    fontFamily: 'Montserrat',
+                    fontSize: 90,
+                    color: containerColor),
               ),
               const SizedBox(
                 height: 60,
@@ -122,10 +162,10 @@ class _MainPageState extends State<MainPage> {
                 child: Text(
                   _textResult,
                   style: const TextStyle(
+                      fontFamily: 'Montserrat',
                       fontSize: 32,
                       fontWeight: FontWeight.w400,
-                      color:warnColor
-                  ),
+                      color: warnColor),
                 ),
               ),
             ],
