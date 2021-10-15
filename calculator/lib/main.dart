9d8e0cb1aa1982d2e877e18090426497aaf46df7
Dmitry <ldimka@gmail.com>
2021-10-15 17:32:55 +0300

    Update main.dart

--- calculator/lib/main.dart
+++ calculator/lib/main.dart
@@ -1,5 +1,22 @@
+import 'package:calculator/page/main.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  runApp(myApp());
+}
 
+
+class myApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Калькулятор',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MainPage(),
+    );
+  }
 }
