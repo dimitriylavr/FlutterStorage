14d200214fa182b810651a26850f06f8ab248591
Dmitry <ldimka@gmail.com>
2021-10-22 17:08:09 +0300

    Update main.dart

--- wallet/lib/main.dart
+++ wallet/lib/main.dart
@@ -4,5 +4,19 @@ void main() {
 
 }
 
+class WalletApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Кошелёчек',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo
+      ),
+      home: MainPage(),
+    );
+  }
+}
+
 
 
