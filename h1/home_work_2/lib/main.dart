340fc3b259738fb82e436a27cda1a55accf69cc4
Dmitry <ldimka@gmail.com>
2021-11-06 20:43:28 +0300

    Update main.dart

--- h1/home_work_2/lib/main.dart
+++ h1/home_work_2/lib/main.dart
@@ -3,15 +3,17 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color(0xffEEEEEE),
+        backgroundColor: const Color(0xffEEEEEE),
         appBar: AppBar(
           title: const Text("Простой счетчки"),
           centerTitle: true,
-          backgroundColor: Color(0xff393E46),
+          backgroundColor: const Color(0xff393E46),
         ),
         body: Row(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -24,7 +26,7 @@ class MyApp extends StatelessWidget {
                   style: TextStyle(fontSize: 18, color: Color(0xff222831)),
                 ),
                 const SizedBox(
-                  height: 10,
+                  height: 20,
                 ),
                 CounterApp(),
                 const SizedBox(
@@ -44,6 +46,7 @@ class MyApp extends StatelessWidget {
 }
 
 class CounterApp extends StatefulWidget {
+  // ignore: prefer_const_constructors_in_immutables
   CounterApp({Key? key}) : super(key: key);
 
   @override
@@ -61,31 +64,39 @@ class _CounterApp extends State<CounterApp> {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        IconButton(
-            onPressed: () {
-              setState(() {
-                _counter--;
-              });
-            },
-            icon: const Image(
-              image: AssetImage("assets/images/minus.png"),
-            )),
-        Text(_counter.toString(),
-            style: const TextStyle(
-                fontSize: 30,
-                color: Color(0xffFD7013),
-                fontWeight: FontWeight.w500)),
-        IconButton(
-            onPressed: () {
-              setState(() {
-                _counter++;
-              });
-            },
-            icon: const Image(
-              image: AssetImage("assets/images/plus.png"),
-            )),
+    return Column(
+      children: [
+        Row(
+          children: <Widget>[
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    _counter--;
+                  });
+                },
+                icon: const Image(
+                  image: AssetImage("assets/images/minus.png"),
+                )),
+            Text(_counter.toString(),
+                style: const TextStyle(
+                    fontSize: 30,
+                    color: Color(0xffFD7013),
+                    fontWeight: FontWeight.w500)),
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    _counter++;
+                  });
+                },
+                icon: const Image(
+                  image: AssetImage("assets/images/plus.png"),
+                )),
+          ],
+        ),
+        Text(
+          _counter >= 0 ? "" : "Минус это плохо, лучше плюс!",
+          style: TextStyle(color: Color(0xffFD7013)),
+        ),
       ],
     );
   }
