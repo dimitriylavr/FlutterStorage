0ad7057d4772834d67ff0f025c6ba93fd82bfeca
Dmitry <ldimka@gmail.com>
2021-11-08 16:47:58 +0300

    Update main.dart

--- udemy3/app3/lib/main.dart
+++ udemy3/app3/lib/main.dart
@@ -25,13 +25,35 @@ class BodyListView extends StatelessWidget {
 }
 
 Widget _myListView() {
-  final List<String> items = List<String>.generate(500, (i) => 'Item $i');
+  final List<ListItem> items = List<ListItem>.generate(
+      500,
+      (i) => i % 6 == 0
+          ? HeadingItem("Heading $i")
+          : MessageItem("Sender $i", "Message body $i"));
 
   return ListView.builder(
       padding: EdgeInsets.all(5),
       itemCount: items.length,
       itemBuilder: (context, index) {
-        return Card(
+        final item = items[index];
+
+        if (item is HeadingItem) {
+          return ListTile(
+            title: Text(
+              item.heading,
+              style: Theme.of(context).textTheme.headline5,
+            ),
+          );
+        } else if (item is MessageItem) {
+          return ListTile(
+            title: Text(item.sender),
+            subtitle: Text(item.body),
+            leading: Icon(Icons.add_a_photo_outlined),
+            trailing: Icon(Icons.keyboard_arrow_right_outlined),
+          );
+        }
+        return SizedBox();
+        /* return Card(
           child: ListTile(
             title: Text("${items[index]}"),
             leading: Icon(
@@ -40,6 +62,19 @@ Widget _myListView() {
             ),
             trailing: Icon(Icons.account_box),
           ),
-        );
+        ); */
       });
+}
+
+abstract class ListItem {}
+
+class MessageItem implements ListItem {
+  final String sender;
+  final String body;
+  MessageItem(this.sender, this.body);
+}
+
+class HeadingItem implements ListItem {
+  final String heading;
+  HeadingItem(this.heading);
 }
