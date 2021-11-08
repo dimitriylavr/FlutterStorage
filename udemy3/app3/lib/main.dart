f1070971ad683b9d790d25a1df2e7a5fc5d19ac4
Dmitry <ldimka@gmail.com>
2021-11-08 16:04:03 +0300

    Update main.dart

--- udemy3/app3/lib/main.dart
+++ udemy3/app3/lib/main.dart
@@ -1,3 +1,34 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("ListView"),
+        ),
+        body: BodyListView(),
+      ),
+    );
+  }
+}
+
+class BodyListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return _myListView();
+  }
+}
+
+Widget _myListView() {
+  return ListView(
+    children: [
+      Text("Sun"),
+      Text("Cloudy"),
+      Text("Snow"),
+    ],
+  );
+}
