176b5ac61a7a41041f3fd87dfbed7fa722975372
Dmitry <ldimka@gmail.com>
2021-11-08 16:14:11 +0300

    Update main.dart

--- udemy3/app3/lib/main.dart
+++ udemy3/app3/lib/main.dart
@@ -25,10 +25,30 @@ class BodyListView extends StatelessWidget {
 
 Widget _myListView() {
   return ListView(
-    children: [
-      Text("Sun"),
-      Text("Cloudy"),
-      Text("Snow"),
+    padding: EdgeInsets.all(10),
+    //scrollDirection: Axis.horizontal,
+    //itemExtent: 200,
+    //reverse: true,
+    children: const [
+      ListTile(
+        title: Text("Sun"),
+        subtitle: Text("data"),
+        leading: Icon(Icons.ac_unit, color: Colors.yellow),
+      ),
+      ListTile(
+        title: Text("Cloudy"),
+        subtitle: Text("data"),
+        leading: Icon(Icons.ac_unit, color: Colors.yellow),
+        trailing: Icon(
+          Icons.keyboard_arrow_right,
+          color: Colors.black,
+        ),
+      ),
+      ListTile(
+        title: Text("Snow"),
+        subtitle: Text("data"),
+        leading: Icon(Icons.ac_unit, color: Colors.yellow),
+      ),
     ],
   );
 }
