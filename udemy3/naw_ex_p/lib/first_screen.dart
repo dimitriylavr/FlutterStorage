02e5fbcec18b401d2a01787a2ecb4173dbab341d
Dmitry <ldimka@gmail.com>
2021-11-09 15:49:10 +0300

    Update first_screen.dart

--- udemy3/naw_ex_p/lib/first_screen.dart
+++ udemy3/naw_ex_p/lib/first_screen.dart
@@ -1,5 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:naw_ex_p/users.dart';
 
+class FirsScreen extends StatefulWidget {
+  @override
+  _FirstScreen createState() => _FirstScreen();
+}
+
+class _FirstScreen extends State<FirsScreen> {
+  String txtName = "Сюда будем принимать текст";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Главное окно")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(50),
+              child: Text(
+                txtName,
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  _returnDataScreen(context);
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red,
+                ),
+                child: Text("Переход далее")),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _returnDataScreen(BuildContext context) async {
+    User user = User(name: "Дмитрий", age: 37);
+    final result =
+        await Navigator.pushNamed(context, "/page2", arguments: user);
+
+    setState(() {
+      txtName = result.toString();
+    });
+  }
+}
+/*
 class FirsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -10,10 +58,13 @@ class FirsScreen extends StatelessWidget {
       ),
       body: Center(
         child: ElevatedButton(
-          onPressed: () {},
+          onPressed: () {
+            User user = User(name: "Dmitrii", age: 37);
+            Navigator.pushNamed(context, "/page2", arguments: user);
+          },
           child: Text("Go to second screen"),
         ),
       ),
     );
   }
-}
+}*/
