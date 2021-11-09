bc4b289f37fd3533f1a6cc9a61a3026f3b9a4ede
Dmitry <ldimka@gmail.com>
2021-11-09 15:49:06 +0300

    Update second_screen.dart

--- udemy3/naw_ex_p/lib/second_screen.dart
+++ udemy3/naw_ex_p/lib/second_screen.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:naw_ex_p/users.dart';
 
 class SecondScreen extends StatelessWidget {
+  TextEditingController txtController = TextEditingController();
   @override
   Widget build(BuildContext context) {
+    final args = ModalRoute.of(context)!.settings.arguments as User;
     return Scaffold(
-      appBar: AppBar(title: Text("Second screen")),
+      appBar: AppBar(title: Text("Ваше имя: ${args.name} - ${args.age}")),
       body: Center(
-        child: ElevatedButton(
-          child: Text("Go back"),
-          onPressed: () {},
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(50),
+              child: TextField(
+                controller: txtController,
+              ),
+            ),
+            ElevatedButton(
+              child: Text("Go back"),
+              onPressed: () {
+                String textBack = txtController.text;
+                Navigator.pop(context, textBack);
+              },
+            ),
+          ],
         ),
       ),
     );
