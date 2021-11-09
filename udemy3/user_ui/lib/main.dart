233d9e4953900675850f1a9067e918592599ac19
Dmitry <ldimka@gmail.com>
2021-11-09 16:32:43 +0300

    Update main.dart

--- udemy3/user_ui/lib/main.dart
+++ udemy3/user_ui/lib/main.dart
@@ -1,3 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:user_ui/pages/register.dart';
 
-void main() {}
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "User form",
+      theme: ThemeData(
+          primarySwatch: Colors.yellow,
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: RegisterPage(),
+    );
+  }
+}
