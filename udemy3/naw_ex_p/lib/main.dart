a48bb631752c38be102b4371c395c9667daf26ff
Dmitry <ldimka@gmail.com>
2021-11-09 15:49:08 +0300

    Update main.dart

--- udemy3/naw_ex_p/lib/main.dart
+++ udemy3/naw_ex_p/lib/main.dart
@@ -1,3 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:naw_ex_p/first_screen.dart';
+import 'package:naw_ex_p/second_screen.dart';
 
-void main() {}
+void main() {
+  runApp(MaterialApp(
+    home: FirsScreen(),
+    routes: {
+      "/page2": (context) => SecondScreen(),
+    },
+  ));
+}
