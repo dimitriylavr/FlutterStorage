bc3ccb4ca6086c50a1f27f1cbf93350e8e9a3945
Dmitry <ldimka@gmail.com>
2021-11-10 14:49:43 +0300

    h2

---
+++ udemy3/home_2/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  _MyApp createState() => _MyApp();
+}
+
+class _MyApp extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
