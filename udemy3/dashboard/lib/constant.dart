b8d0af65661f4c887f99788ff6d9f6c7e6a0b89f
Dmitry <ldimka@gmail.com>
2021-11-12 23:45:15 +0300

    Update constant.dart

--- udemy3/dashboard/lib/constant.dart
+++ udemy3/dashboard/lib/constant.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
+const mainColor = Color(0xFF17325c);
 const primaryColor = Color(0xFF2697FF);
 const secondaryColor = Color(0xFF2A2D3E);
-const bgColor = Color(0xFF212332);
+const bgColor = Color(0xFF17325c);
+const whiteColor = Color(0xFFEEEEEE);
 
 const defaultPadding = 16.0;
