7aeceb847530dd172cfc89e1f3b7e4aac0261f84
Dmitry <ldimka@gmail.com>
2021-11-12 23:45:28 +0300

    Create main.dart

---
+++ udemy3/dashboard/lib/screen/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'dashboard.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              flex: 5,
+              child: DashboardScreen(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
