55320b0b56f9b3374aa33987078e002813e90a73
Dmitry <ldimka@gmail.com>
2021-11-15 17:30:23 +0300

    Update constant.dart

--- udemy3/dashboard/lib/constant.dart
+++ udemy3/dashboard/lib/constant.dart
@@ -5,5 +5,6 @@ const primaryColor = Color(0xFF2697FF);
 const secondaryColor = Color(0xFF2A2D3E);
 const bgColor = Color(0xFF17325c);
 const whiteColor = Color(0xFFEEEEEE);
+const redColor = Color(0xFFbf2038);
 
 const defaultPadding = 16.0;
